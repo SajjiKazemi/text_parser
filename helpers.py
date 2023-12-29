ac45611da4eb7a4ba306678e9497f811c35d7984
sajji <sajjadkazemi.e@gmail.com>
2023-12-29 14:02:23 -0500

    Add the function to define a line from two points

--- helpers.py
+++ helpers.py
@@ -10,3 +10,11 @@ def parse_input(line: str):
     street_name = inputs[1]
     vertices = re.split('\s', inputs[2].strip())
     return cmd, street_name, vertices
+
+def create_line(point1, point2):
+    """Create a line from two points, considering the line equation Ax + By + C = 0"""
+    A = (point1[1] - point2[1])
+    B = (point2[0] - point1[0])
+    C = (point1[0]*point2[1] - point2[0]*point1[1])
+    return A, B, -C
+    
