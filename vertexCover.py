85cdacdcc0db4c4c18bf2b7b39013bb5a454e8c9
sajji <sajjadkazemi.e@gmail.com>
2023-12-29 14:08:51 -0500

    Add two methods
    In order to find vertices and intersections according to the definition.
    Then, remove the repeated vertices
    and make it ready for finding the edges.

--- vertexCover.py
+++ vertexCover.py
@@ -6,7 +6,8 @@ from helpers import *
 
 class vertexCover:
     def __init__(self):
-        self.vertices = []
+        self.vertices = {}
+        self.intersections = {}
         self.edges = []
         self.streets = {}
     
@@ -18,7 +19,15 @@ class vertexCover:
             vertices.append((x_arg, y_arg))
         self.streets[name] = vertices
 
-    def get_intersecions(self):
+    def add_edge(self, vertex1_index, vertex2_index):
+        self.edges.append(f"<{vertex1_index},{vertex2_index}>")
+        return
+
+    def add_vertex(self, vertex):
+        index = len(self.vertices)
+        self.vertices[index] = vertex
+
+    def update_vertices(self):
         keys = list(self.streets.keys())
         while len(keys) > 1:
             street1 = keys[0]
@@ -39,13 +48,62 @@ class vertexCover:
                                 if intersection_point and check_intersection(
                                     intersection_point, end1_line1, end2_line1
                                     , end1_line2, end2_line2):
-                                    self.vertices.append(end1_line1)
-                                    self.vertices.append(end2_line1)
-                                    self.vertices.append(end1_line2)
-                                    self.vertices.append(end2_line2)
-                                    self.vertices.append(intersection_point)
-        self.vertices = list(set(self.vertices))
-        return self.vertices
-    
+                                    self.vertices[len(self.vertices)+1] = end1_line1
+                                    self.vertices[len(self.vertices)+1] = end2_line1
+                                    self.vertices[len(self.vertices)+1] = end1_line2
+                                    self.vertices[len(self.vertices)+1] = end2_line2
+                                    self.vertices[len(self.vertices)+1] = intersection_point
+                                    vertices_indices = list(self.vertices.keys())
+                                    self.intersections[f'intn{len(self.intersections)}v{int((vertices_indices)[-1])}'] = \
+                                        [int((vertices_indices)[-5]), int((vertices_indices)[-4])\
+                                         , int((vertices_indices)[-3]), int((vertices_indices)[-2])]
+        #self.vertices = list(set(self.vertices))
+        self.organize_intersections()       # Organize vertices (inside organize_intersections)
+                                            # and intersections
+
+
+    def organize_vertices(self):
+        intermediate = {}
+        for vertex in self.vertices:
+            if self.vertices[vertex] not in intermediate.values():    
+                intermediate[len(intermediate)+1] = self.vertices[vertex]
+        return intermediate
+
+    def organize_intersections(self):
+        intermediate = self.organize_vertices()
+        for vertex in self.vertices:
+            repeated_vertex = self.vertices[vertex]
+            for vertex2 in intermediate:
+                if intermediate[vertex2] == repeated_vertex:
+                    key_vertex2 = vertex2
+                    for intersection in self.intersections:
+                        if vertex in self.intersections[intersection]:
+                            vertex_index = self.intersections[intersection].\
+                                index(vertex)
+                            self.intersections[intersection][vertex_index] = key_vertex2
+
+        intersection_keys = list(self.intersections.keys())
+        for intersection_key in intersection_keys:
+            value_in_vertices = self.vertices[int(re.split("[v]", intersection_key)[1])]
+            for key_intermediate, value_intermediate in intermediate.items():
+                if value_in_vertices == value_intermediate and key_intermediate !=\
+                      int(re.split("[v]", intersection_key)[1]):
+                        value_to_change = self.intersections[intersection_key]
+                        self.intersections[f'intn{len(self.intersections)}v{key_intermediate}']\
+                              = value_to_change
+                        self.intersections[intersection_key] = None
+        intersection_keys = list(self.intersections.keys())
+        for intersection_key in intersection_keys:
+            if self.intersections[intersection_key] == None:
+                del self.intersections[intersection_key]
+        self.vertices = intermediate
+
+    def get_edges(self):
+        keys = list(self.intersections.keys())
+        if len(keys) < 1:
+            return []
+        if len(keys) == 1:
+            return [f"<0,1>"] 
+
     def get_streets(self):
         return self.streets
