77e732bf6d1aad9515c556edf7b0968da0b18005
sajji <sajjadkazemi.e@gmail.com>
2023-12-29 14:11:52 -0500

    Add necessary lines of code
    To get the "add" and "gg" comands.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 import sys
+import re
+
+from helpers import *
+from vertexCover import *
 
 def main():
+
+    # Create an instance of vertexCover
+    vertex_cover = vertexCover()
+
     # Read input from stdin and print it to stdout
     # Note: input is terminated by EOF (Ctrl+D on Linux)
     while True:
@@ -9,7 +17,18 @@ def main():
         if line == "":
             break
         print("reading a line:", line)
+        
+       # split the line into words
+        cmd, street_name, vertices = parse_input(line)
 
+        if cmd == "add":
+            vertex_cover.add_street(street_name, vertices)
+            print(vertex_cover.get_streets())
+            print("Added a street")
+        elif cmd == "gg":
+            vertex_cover.update_vertices()
+
+        
     print("Finished reading input")
     sys.exit(0)
 
